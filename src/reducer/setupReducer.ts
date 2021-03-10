371caf0e1c221b5dc3a6c1d8cb736dc7ed9b2fbd
Pavel Ostashkin <ostashpav@gmail.com>
2021-03-10 18:30:54 +0300

    feat: setupReducer.ts
    #10

--- src/reducer/setupReducer.ts
+++ src/reducer/setupReducer.ts
@@ -1,5 +1,13 @@
-import { AnyStateHandler, Reducer, ReducerCreator } from '../types/reducer';
+import { AnyStateHandler, PayloadToStateHandler, Reducer, ReducerCreator } from '../types/reducer';
 import { UnknownAction } from '../types/common';
+import { BasicActionCreator } from '../types/creator';
+
+const getCreatorsTypes = <ActionType extends string>(
+  ...creators: BasicActionCreator<ActionType>[]
+): ActionType[] => {
+  const ejectType = ({ type }: BasicActionCreator<ActionType>) => type;
+  return creators.map(ejectType);
+};
 
 export function setupReducer<State, Action extends UnknownAction<string>>(
   initialState: State
@@ -13,6 +21,56 @@ export function setupReducer<State, Action extends UnknownAction<string>>(
         const { [action.type]: handler } = handlers;
         return handler(state, action.payload);
       };
+    },
+
+    on(
+      actionCreatorOrActionCreators: BasicActionCreator<string> | BasicActionCreator<string>[],
+      staticValueOrHandler: State | AnyStateHandler<State>
+    ) {
+      const registerHandler = ({ type }: BasicActionCreator<string>): void => {
+        handlers[type] =
+          typeof staticValueOrHandler === 'function'
+            ? (staticValueOrHandler as AnyStateHandler<State>)
+            : () => staticValueOrHandler;
+      };
+      if (Array.isArray(actionCreatorOrActionCreators)) {
+        actionCreatorOrActionCreators.forEach(registerHandler);
+        return reducerCreator;
+      }
+      registerHandler(actionCreatorOrActionCreators);
+      return reducerCreator;
+    },
+
+    reset(...actionCreators) {
+      const types = getCreatorsTypes(...actionCreators);
+      types.forEach((type) => {
+        handlers[type] = () => initialState;
+      });
+      return reducerCreator;
+    },
+
+    switchToPayload(...actionCreators) {
+      const types = getCreatorsTypes(...actionCreators);
+      types.forEach((type) => {
+        handlers[type] = (_state, payload) => payload;
+      });
+      return reducerCreator;
+    },
+
+    getFromPayload<ActionCreator extends BasicActionCreator<string>>(
+      actionCreatorOrActionCreators: ActionCreator | ActionCreator[],
+      handler: PayloadToStateHandler<State>
+    ) {
+      const registerHandler = ({ type }: ActionCreator): void => {
+        handlers[type] = (_state, payload) => handler(payload);
+      };
+
+      if (Array.isArray(actionCreatorOrActionCreators)) {
+        actionCreatorOrActionCreators.forEach(registerHandler);
+        return reducerCreator;
+      }
+      registerHandler(actionCreatorOrActionCreators);
+      return reducerCreator;
     },
   };
   return reducerCreator;
--- src/types/reducer.ts
+++ src/types/reducer.ts
@@ -1,4 +1,5 @@
-import { BasicAction, UnknownAction } from './common';
+import { BasicAction, PayloadFunction, UnknownAction } from './common';
+import { ActionCreatorBase, ActionCreatorWithPayload } from './creator';
 
 export type Reducer<State = any, Action extends UnknownAction<string> = BasicAction<string>> = (
   state: State | undefined,
@@ -7,18 +8,99 @@ export type Reducer<State = any, Action extends UnknownAction<string> = BasicAct
 
 export type EmptyStateHandler<State> = () => State;
 export type StateHandler<State> = (state: State) => State;
-export type FullStateHandler<State, Payload> = (state: State, payload: Payload) => State;
+export type StateWithPayloadHandler<State, Payload> = (state: State, payload: Payload) => State;
 
 export type AnyStateHandler<State, Payload = any> =
   | EmptyStateHandler<State>
   | StateHandler<State>
-  | FullStateHandler<State, Payload>;
+  | StateWithPayloadHandler<State, Payload>;
+
+export interface PayloadToStateHandler<State, Payload = any> {
+  (payload: Payload): State;
+}
+
+export interface ReducerActionHandler<State, Action extends BasicAction<string>> {
+  // Single Action Creator
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreator: ActionCreator,
+    staticValue: State
+  ): ReducerCreator<State, Action>;
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreator: ActionCreator,
+    handler: () => State
+  ): ReducerCreator<State, Action>;
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreator: ActionCreator,
+    handler: (state: State) => State
+  ): ReducerCreator<State, Action>;
+  <
+    ActionType extends Action['type'],
+    ActionCreator extends ActionCreatorWithPayload<ActionType, any>
+  >(
+    actionCreator: ActionCreator,
+    handler: (state: State, payload: ReturnType<ActionCreator>['payload']) => State
+  ): ReducerCreator<State, Action>;
+
+  // Multiple Action Creators
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreators: ActionCreator[],
+    staticValue: State
+  ): ReducerCreator<State, Action>;
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreators: ActionCreator[],
+    handler: () => State
+  ): ReducerCreator<State, Action>;
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    actionCreators: ActionCreator[],
+    handler: (state: State) => State
+  ): ReducerCreator<State, Action>;
+  <
+    ActionType extends Action['type'],
+    ActionCreator extends ActionCreatorWithPayload<ActionType, any>
+  >(
+    actionCreators: ActionCreator[],
+    handler: (state: State, payload: ReturnType<ActionCreator>['payload']) => State
+  ): ReducerCreator<State, Action>;
+}
+
+export interface ReducerResetHandler<State, Action extends UnknownAction<string>> {
+  <ActionType extends Action['type'], ActionCreator extends ActionCreatorBase<ActionType>>(
+    ...actions: ActionCreator[]
+  ): ReducerCreator<State, Action>;
+}
+
+export interface ReducerSwitchToPayloadHandler<State, Action extends UnknownAction<string>> {
+  <ActionType extends Action['type']>(
+    ...actionCreators: ActionCreatorWithPayload<ActionType, PayloadFunction<State>>[]
+  ): ReducerCreator<State, Action>;
+}
+
+export interface ReducerGetFromPayloadHandler<State, Action extends UnknownAction<string>> {
+  // Single Action Creator
+  <
+    ActionType extends Action['type'],
+    ActionCreator extends ActionCreatorWithPayload<ActionType, any>
+  >(
+    actionCreator: ActionCreator,
+    handler: PayloadToStateHandler<State, ReturnType<ActionCreator>['payload']>
+  ): ReducerCreator<State, Action>;
+
+  // Multiple Action Creators
+  <
+    ActionType extends Action['type'],
+    ActionCreator extends ActionCreatorWithPayload<ActionType, PayloadFunction<State>>
+  >(
+    actionCreators: ActionCreator[],
+    handler: PayloadToStateHandler<State, ReturnType<ActionCreator>['payload']>
+  ): ReducerCreator<State, Action>;
+}
 
 export interface ReducerCreator<State, Action extends BasicAction<string> = BasicAction<string>> {
   create(): Reducer<State, Action>;
-  // on: ReducerActionHandler<State, Action>;
-  // reset: ReducerResetHandler<State, Action>;
-  // switchToPayload: ReducerSwitchToPayloadHandler<State, Action>;
+  on: ReducerActionHandler<State, Action>;
+  reset: ReducerResetHandler<State, Action>;
+  switchToPayload: ReducerSwitchToPayloadHandler<State, Action>;
+  getFromPayload: ReducerGetFromPayloadHandler<State, Action>;
   // injectLogic: InjectLogic<State, Action>;
 }
 
